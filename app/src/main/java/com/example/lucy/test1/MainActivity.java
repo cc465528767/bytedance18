803ff094f7ebfba8e92336efbb98e93946774d8e
郑笑寒 <lucy@zhengxiaohandeMacBook-Pro.local>
2019-01-18 17:19:06 +0800

    第二次提交

--- app/src/main/java/com/example/lucy/test1/MainActivity.java
+++ app/src/main/java/com/example/lucy/test1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lucy.test1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(intent);
+                Log.d("lalala", "我跳过去了");
 
             }
         });
